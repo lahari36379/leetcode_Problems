d8640ab5bb4cc3c62f4416f3edb3562b043d85ea
lahari36379 <lahari36379@gmail.com>
2026-08-02 18:26:17 +0530

    Time: 14 ms (57.20%) | Memory: 20 MB (20.63%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -6,23 +6,18 @@ class ParkingSystem:
         self.small=small
     def addCar(self, carType: int) -> bool:
         if carType==1:
-            if self.big>0:
-                self.big-=1
-                return True
-            else:
-                return False
+           if self.big>0:
+            self.big-=1
+            return True
         elif carType==2:
             if self.medium>0:
                 self.medium-=1
                 return True
-            else:
-                return False
         else:
             if self.small>0:
                 self.small-=1
                 return True
-            else:
-                return False                               
+        return False                                        
         
 
 
