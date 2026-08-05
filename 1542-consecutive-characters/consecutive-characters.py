6e5f71035c44cac29d7e038ed7e149aa951251ee
lahari36379 <lahari36379@gmail.com>
2026-08-05 10:34:23 +0530

    Time: 4 ms (46.80%) | Memory: 19.2 MB (52.07%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.py
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -2,8 +2,8 @@ class Solution:
     def maxPower(self, s: str) -> int:
         count=1
         max_count=1
-        for i in range(1,len(s)):
-            if s[i-1]==s[i]:
+        for i in range(len(s)-1):
+            if s[i]==s[i+1]:
                 count+=1
             else:
                 if count>max_count:
