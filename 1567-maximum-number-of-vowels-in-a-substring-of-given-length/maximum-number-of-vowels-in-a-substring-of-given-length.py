d0064aa030d9f88496cdbd837a2561c1af85328d
lahari36379 <lahari36379@gmail.com>
2026-08-06 12:01:57 +0530

    Time: 63 ms (35.75%) | Memory: 19.9 MB (11.55%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,16 +1,20 @@
+def is_v(ch):
+    return ch in "aeiou"
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        max_vowel_count=0
-        left=0
-        c=0
-        for right in range(len(s)):
-            if s[right] in "aeiou":
-                c+=1
-            if right>=k-1:
-                max_vowel_count=max(max_vowel_count,c)
-                if s[left] in "aeiou":
-                    c-=1
-                left+=1
-        return max_vowel_count           
-
+        #compute the no of vowels in the first k-size substring
+        first_window=s[:k]
+        v_c=0
+        for i in first_window:
+            if is_v(i):
+                v_c+=1
+        mx_v=max(0,v_c)
+        #sliding window logic
+        for i in range(k,len(s)):
+            if is_v(s[i]):  #New element
+                v_c+=1  
+            if is_v(s[i-k]):  #Leaving element
+                v_c-=1 
+            mx_v=max(mx_v,v_c) 
+        return mx_v                  
         
