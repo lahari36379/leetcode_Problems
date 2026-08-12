7b428033f2d9430ccddcbedde8029b49c52fbca8
lahari36379 <lahari36379@gmail.com>
2026-08-12 22:20:43 +0530

    Time: 19 ms (60.31%) | Memory: 19.3 MB (86.18%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,12 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         d1={}
-        for i in range(len(s1)):
-            d1[s1[i]]=d1.get(s1[i],0)+1
-        d2={}
+        for i in s1:
+            if i in d1.keys():
+                d1[i]+=1
+            else:
+                d1[i]=1
         left=0
+        d2={}
         for right in range(len(s2)):
-            d2[s2[right]]=d2.get(s2[right],0)+1
+            if s2[right] in d2.keys():
+                d2[s2[right]]+=1
+            else:
+                d2[s2[right]]=1
             if right>=len(s1)-1:
                 if d1==d2:
                     return True
@@ -15,6 +21,6 @@ class Solution:
                 if d2[s2[left]]==0:
                     d2.pop(s2[left])
                 left+=1
-        return False                
+        return False                 
 
         
