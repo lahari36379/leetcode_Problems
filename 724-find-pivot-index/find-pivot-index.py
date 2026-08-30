88319c38e842c888d4f6849dd31c1d3cb7f5460c
lahari36379 <lahari36379@gmail.com>
2026-08-30 16:53:42 +0530

    Time: 8 ms (38.22%) | Memory: 20.2 MB (97.30%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -6,8 +6,8 @@ class Solution:
             sum_+=nums[i]
             prefix_array.append(sum_)     
         for i in range(len(nums)):
-            left_sum=prefix_array[i]
-            right_sum=prefix_array[len(nums)]-prefix_array[i+1]
+            left_sum=prefix_array[i-1+1]
+            right_sum=prefix_array[len(prefix_array)-1]-prefix_array[i+1]
             if left_sum==right_sum:
                 return i
         return -1        
